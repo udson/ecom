bf37bcbb38a387accd5ce8e6373f3069f214293f
Udson Silva <udson2112@outlook.com>
2023-12-12 22:08:09 -0300

    Add MDB.

--- store/views.py
+++ store/views.py
@@ -5,27 +5,27 @@ from .models import Product
 
 
 def home(request):
-    products = Product.objects.all()
-    return render(request, 'store/home.html', {'products': products})
+  products = Product.objects.all()
+  return render(request, 'store/mdb/home.html', {'products': products})
 
 def login_user(request):
-    if request.user.is_authenticated:
-        return redirect('home')
+  if request.user.is_authenticated:
+    return redirect('home')
 
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, message='Welcome!')
-            return redirect('home')
-        else:
-            messages.error(request, message='Wrong username or password. Try again.')
-            return render(request, 'store/login.html', {})
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      messages.success(request, message='Welcome!')
+      return redirect('home')
     else:
-        return render(request, 'store/login.html', {})
+      messages.error(request, message='Wrong username or password. Try again.')
+      return render(request, 'store/login.html', {})
+  else:
+    return render(request, 'store/login.html', {})
 
 def logout_user(request):
-    logout(request)
-    return redirect('home')
+  logout(request)
+  return redirect('home')
