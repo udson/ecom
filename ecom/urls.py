3720c29f327bcd86633d1ec18e9b49cb32f443e6
Udson Silva <udson2112@outlook.com>
2023-12-20 20:25:40 -0300

    Add cart app.

--- ecom/urls.py
+++ ecom/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('store.urls')),
+    path('cart/', include('cart.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
